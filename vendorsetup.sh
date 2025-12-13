4c1f67036091a88dac51ad0a7681a8d1b11446f1
Kitchannel2010 <84881541+Kitchannel2010@users.noreply.github.com>
2025-12-13 23:22:41 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,8 +17,6 @@ fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 
-export FOX_VERSION="R12.1"
-export FOX_VARIANT="A14"
 export FOX_BUILD_TYPE="Stable"
 export FOX_ENABLE_APP_MANAGER=1
 export FOX_USE_BASH_SHELL=1
